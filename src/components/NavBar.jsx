42c4041481c5867a4bb1e4a32e9ddcf9d5eb3eb2
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2021-11-30 09:58:17 -0300

    Desafio completado, el logo funcionaria como boton de inicio, por ahora los redireccione todos a youtube para que tengan un valor y no salte warnings

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+
+import "./NavBar.css"
+
+function NavBar (){
+    return(
+        <nav className="navBar">
+            <a className="logo" href="https://www.youtube.com/"><h2>ataraxia</h2></a>
+            <div className="navigation">
+                <a href="https://www.youtube.com/">hits</a>
+                <a href="https://www.youtube.com/">accesios</a>
+                <a href="https://www.youtube.com/">sets</a>
+                <a href="https://www.youtube.com/">sale</a>
+                <a href="https://www.youtube.com/">shop</a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
